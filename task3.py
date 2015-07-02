07e9328020d9e08f8da36e2cd8ca980af7787090
Challlanger <gerony81@gmail.com>
2015-07-02 17:12:03 +0300

    Provali v pamati

--- task3.py
+++ task3.py
@@ -1,10 +1,12 @@
 import sys
 import random
-def compute ( n ):
-    i = 0; s = 0
+
+def compute(n):
+    i = 0
+    s = 0
     while i <= n:
-        s += random . random ()
+        s += random . random()
         i += 1
-    return s /n
-n = sys . argv [1]
-print "average of %d random numbers is %g " % (n , compute (n ))
+    return s/n
+n = sys . argv[1]
+print "average of %d random numbers is %g " % (n, compute(n))
