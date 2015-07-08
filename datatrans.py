fd7da7720a29235975e53efeb715adc137a4e5c9
Challlanger <gerony81@gmail.com>
2015-07-08 14:58:05 +0300

    Finish datatrans.py

--- datatrans.py
+++ datatrans.py
@@ -2,27 +2,23 @@ import sys
 import math
 
 try:
-    infilename = sys.argv[1]
-    outfilename = sys.argv[2]
+    outfilename = sys.argv[1]
 except:
-    print "Usage:", sys.argv[0], "infile  outfile"
+    print "Usage:", sys.argv[0], "outfile"
     sys.exit(1)
+ofile = open(outfilename, 'w')
 
-ifile = open(infilename , ’r’)
-ofile = open(outfilename , ’w’)
-
-def  myfunc(y):
-    if y  >= 0.0:
+def myfunc(y):
+    if y >= 0.0:
         return y**5*math.exp(-y)
     else:
-        return  0.0
+        return 0.0
 
-for  line in ifile:
-    pair = line.split()
+for i in range(2, len(sys.argv), 2):
+    pair = sys.argv[i:i+2]
     x = float(pair[0])
     y = float(pair[1])
-    fy = myfunc(y)
-    ofile.write(’%g %12.5e\n’ % (x,fy))
+    ofile.write('%g %12.5e\n' % (x, myfunc(y)))
+    i += 2
 
-ifile.close()
 ofile.close()
