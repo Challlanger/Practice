67a246b80dd30af7e5d74c81264f9c03de2bc38f
Challlanger <gerony81@gmail.com>
2015-07-08 13:12:41 +0300

    version2  task4.py

--- task4.py
+++ task4.py
@@ -1,6 +1,7 @@
 import sys
 import math
 
+
 def version1():
     for arg in sys.argv[1:]:
         result = -1
@@ -8,5 +9,15 @@ def version1():
             result = math.log(float(arg))
         except ValueError:
             pass
-            print("log({0}) is {1}".format(arg, "incorrect" if result == -1 else result))
+        print("ln({0}) is {1}".format(arg, "incorrect" if result == -1 else result))
 
+
+def version2():
+    for i in range(1, len(sys.argv)):
+        result = -1
+        try:
+            result = math.log(float(sys.argv[i]))
+        except ValueError:
+            pass
+        print("ln({0}) is {1}".format(sys.argv[1], "incorrect" if result == -1 else result))
+        i += 1
